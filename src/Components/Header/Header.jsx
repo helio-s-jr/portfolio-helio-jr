4f1d0fa0bbb692b64d4f56335a49ccc2638fa8d4
Helio Junior <heliojr.zpt@gmail.com>
2022-11-25 17:30:41 -0300

    Atualização header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import * as S from './Style'
+import Logo from '../../Assets/Logo/logo.png'
 
 const Header = () => {
     return(
-        <section>
-            <Link to='/'>Home</Link>
-            <Link to='/bio'>Biografia</Link>
-            <Link to='/projects'>Projetos</Link>
-            <Link to='/social'>Social</Link>
-            <Link to='/contact'>Contato</Link>
-        </section>
+        <S.ContainerHeader>
+            <figure>
+                <img src={Logo} alt="Ilustração Logo HelioJr Dev" />
+            </figure>
+            <div>
+                <h1>Helio Jr Dev</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link class= 'linkHeader' to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/bio'>Bio</Link>
+                    </li>
+                    <li>
+                        <Link to='/projects'>Projetos</Link>
+                    </li>
+                    <li>
+                        <Link to='/social'>Social</Link>
+                    </li>
+                    <li>
+                        <Link to='/contact'>Contato</Link>
+                    </li>
+                </ul>
+            </nav>
+        </S.ContainerHeader>
     );
 }
 
---
+++ src/Components/Header/Style.js
@@ -0,0 +1,10 @@
+import style from 'styled-components'
+
+export const ContainerHeader = style.section`
+
+    width: 100%;
+    margin: 0auto;
+    display: flex;
+    justify-content: space-evenly;
+    background-color: grey;
+`
